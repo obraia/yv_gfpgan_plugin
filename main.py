a2ea557a4882f4c84d5203249a2415f778d32e16
obraia <obraiadev@gmail.com>
2023-05-15 16:21:44 -0300

    fix: remove weight

--- main.py
+++ main.py
@@ -8,9 +8,7 @@ model_dir = os.path.join(os.path.dirname(__file__), 'gfpgan', 'weights')
 def handler(data, properties):
     model = properties.get('model')
     upscale = properties.get('upscale')
-    weight = properties.get('weight')
     pretrain_model_path = os.path.join(model_dir, model)
-
 
     for item in data:
       image = cv2.imread(os.path.join(images_dir, item['image']))
@@ -30,7 +28,7 @@ def handler(data, properties):
       )
 
       _, _, restored_image = restorer.enhance(
-        image, has_aligned=False, only_center_face=False, paste_back=True, weight=float(weight)
+        image, has_aligned=False, only_center_face=False, paste_back=True
       )
 
       cv2.imwrite(os.path.join(images_dir, item['image']), restored_image)
